0cecf2f93b0080dd509da2750a3264d3cd0127ca
SAGOR690beep <jahidul2782466@gmail.com>
2025-05-08 18:36:16 +0600

    Update pending.js

--- models/commands/pending.js
+++ models/commands/pending.js
@@ -22,7 +22,7 @@ module.exports.languages = {
     "en": {
         "invaildNumber": "%1 is not an invalid number",
         "cancelSuccess": "Refused %1 thread!",
-        "notiBox": "Joy BoT Connected Successfully!\nUse +help for more info :>",
+        "notiBox": "SaGor BoT Connected Successfully!\nUse +help for more info :>",
         "approveSuccess": "Approved successfully %1 threads!",
 
         "cantGetPendingList": "Can't get the pending list!",
